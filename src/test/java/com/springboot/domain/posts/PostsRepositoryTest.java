7880e9879b00f1fd63047a22ca3b371b5aa4f1ae
user <chldbfkr456@naver.com>
2023-10-26 14:06:15 +0900

    2023-10-26 Commit - 등록일과 수정일 로그를 남길 수 있는 기능 클래스 BaseTimeEntity 생성. JPA Auditing 적용

--- src/test/java/com/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.springboot.domain.posts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+@Slf4j
 @SpringBootTest
 public class PostsRepositoryTest  {
     @Autowired
@@ -41,6 +44,28 @@ public class PostsRepositoryTest  {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void registerBaseTimeEntity() {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        System.out.println(">>> createDate=" + posts.getCreatedTime() + " modifiedDate="
+                + posts.getModifiedTime());
+
+        assertThat(posts.getCreatedTime()).isAfter(now);
+        assertThat(posts.getModifiedTime()).isAfter(now);
 
     }
 }
