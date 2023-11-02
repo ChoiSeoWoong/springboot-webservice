f17c5d65b830d17133ea1c134b895641e2c8d628
user <chldbfkr456@naver.com>
2023-11-02 20:15:21 +0900

    2023-11-02 Commit - Naver 로그인 api 연동, test에 application.properties가 적용되도록 수정, controller test 코드 수정

--- src/test/java/com/springboot/web/HelloControllerTest.java
+++ src/test/java/com/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.springboot.web;
 
+import com.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class) // 컨트롤러만 사용하기 때문에 선언
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+        classes = SecurityConfig.class)}) // 컨트롤러만 사용하기 때문에 선언
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void returnHello() throws Exception {
         String hello = "hello";
 
@@ -27,6 +34,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void returnHelloDto() throws Exception {
         String name = "hello";
         int amount = 1000;
